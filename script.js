1d35d4f55b0705f7ac225728ffc9197dfac68212
Tiago Sansão <tiagossansao@gmail.com>
2021-02-14 22:16:54 -0300

    Finished toggle's script

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Dark theme
 
+const darkInput = document.querySelector("input#checkbox");
+darkInput.addEventListener("change", () => {
+  document.body.classList.toggle("light");
+  document.body.classList.toggle("dark");
+});
