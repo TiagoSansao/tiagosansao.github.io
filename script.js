eb81822e8951aa9e55402232d727ea493e804c9e
Tiago Sansão <tiagossansao@gmail.com>
2021-02-16 21:27:10 -0300

    Created aside menu

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ darkInput.addEventListener("change", () => {
   document.body.classList.toggle("light");
   document.body.classList.toggle("dark");
 });
+
+// Navbar menu
+
+const menuButton = document.querySelector(".menuButton");
+menuButton.addEventListener("click", () => {
+  document.querySelector(".menuAside").classList.toggle("open")
+})
+
+const closeButton = document.querySelector(".closeButton");
+closeButton.addEventListener("click", () => {
+  document.querySelector(".menuAside").classList.remove("open")
+})
