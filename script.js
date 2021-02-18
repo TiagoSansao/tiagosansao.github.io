e7ec93754a7cbc00dab5e54a2567106c619d1bf8
tiago <tiagossansao@gmail.com>
2021-02-18 19:43:04 -0300

    Added animation when aside menu opens

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ darkInput.addEventListener("change", () => {
 const menuButton = document.querySelector(".menuButton");
 menuButton.addEventListener("click", () => {
   document.querySelector(".menuAside").classList.toggle("open")
+  document.body.classList.toggle("shadowed");
 })
 
 const closeButton = document.querySelector(".closeButton");
 closeButton.addEventListener("click", () => {
   document.querySelector(".menuAside").classList.remove("open")
+  document.body.classList.remove("shadowed");
 })
