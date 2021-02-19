749d65bbfb665a1078cb90ccfdf35eed863894a9
tiago <tiagossansao@gmail.com>
2021-02-18 21:01:00 -0300

    Added event listener to close the side menu when a user clicks anywhere on the site

--- script.js
+++ script.js
@@ -12,10 +12,18 @@ const menuButton = document.querySelector(".menuButton");
 menuButton.addEventListener("click", () => {
   document.querySelector(".menuAside").classList.toggle("open")
   document.body.classList.toggle("shadowed");
-})
+});
 
 const closeButton = document.querySelector(".closeButton");
 closeButton.addEventListener("click", () => {
-  document.querySelector(".menuAside").classList.remove("open")
+  document.querySelector(".menuAside").classList.remove("open");
   document.body.classList.remove("shadowed");
-})
+});
+
+window.addEventListener("click", (event) => {
+  console.log(event.target.matches('.menuButton'));
+  if (!event.target.matches('.menuButton')) {
+    document.querySelector(".menuAside").classList.remove("open");
+    document.body.classList.remove("shadowed");
+  }
+});
