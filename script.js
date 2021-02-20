48f2c5191baa069a5d89905e4f756ba93ceff51b
tiago <tiagossansao@gmail.com>
2021-02-20 09:31:19 -0300

    Added paragraph saying if dark mode is on in both switch containers

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 // Dark theme
 
+const darkState = document.querySelectorAll(".dark-state");
 let theme = localStorage.getItem('theme-ts');
 theme === null ? theme = "dark" : "";
 document.body.classList.add(theme);
+darkState.forEach((element) => {
+  element.innerHTML = theme === "dark" ? "<span class='on'>ON</span>" : "<span class='off'>OFF</span>";
+});
 
 const darkInput = document.querySelectorAll(".checkbox");
 [...darkInput].map((element) => {
   element.addEventListener("change", () => {
     document.body.classList.toggle("light");
     let isDark = document.body.classList.toggle("dark");
+    darkState.forEach((element) => {
+      element.innerHTML = isDark ? "<span class='on'>ON</span>" : "<span class='off'>OFF</span>";
+    });
    localStorage.setItem('theme-ts', isDark ? "dark" : "light");
   });
 });
