f55b56a0078ef2eb7534399d2a2bbc761756141b
tiago <tiagossansao@gmail.com>
2021-02-25 12:35:17 -0300

    Finished the feature that highlight in the navbar the section you are in

--- script.js
+++ script.js
@@ -45,13 +45,18 @@ window.addEventListener("click", (event) => {
 
 const skills = document.querySelectorAll(".technology");
 const navItems = document.querySelectorAll(".headerNav>a")
-let navSectionObj = {};
-console.log(navSectionObj);
+const menuNavItems = document.querySelectorAll(".menuAside>a")
+let navSectionArr = [];
 
 navItems.forEach((element) => {
   const navHrefElement = document.querySelector(element.getAttribute("href"));
-  navSectionObj[element.textContent] = {nav: element, section: navHrefElement};
+  navSectionArr.push([navHrefElement, element])
 });
+
+menuNavItems.forEach((element, index) => {
+  navSectionArr[index].push(element);
+});
+
 
 let before = [-1, null]
 
@@ -59,7 +64,6 @@ function checkIfElementIsInViewport() {
   const windowHeight = window.innerHeight;
   const scrollY = window.scrollY || window.pageYOffset;
   const scrollPosition = scrollY + windowHeight;
-  let navItemsArray = ["Home", "Skills", "Projects", "Contact"]
 
   skills.forEach((element) => {
     const elPosition = element.getBoundingClientRect().top + scrollY  + element.clientHeight;
@@ -68,19 +72,20 @@ function checkIfElementIsInViewport() {
     }
   });
 
-  for (let item in navSectionObj) {
-    const currentElement = navSectionObj[item];
-    const currentElementId = navItemsArray.indexOf(item);
-    const previousElement = navSectionObj[navItemsArray[currentElementId - 1]] || null;
-    if (previousElement) {
-      navSectionObj["Home"].nav.classList.remove('active');
-      if (previousElement.section.getBoundingClientRect().top < 0 && currentElement.section.getBoundingClientRect().top > 0) {
-        console.log(item);
-        console.log(previousElement.section.getBoundingClientRect().top);
-      }
-    } else {
-      navSectionObj["Home"].nav.classList.add('active');
+  let highlightedNavIndex = 0;
+
+  for (let i = 0; i < navSectionArr.length; i+= 1) {
+    const highlightedNavIndexDistanceFromTop = navSectionArr[highlightedNavIndex][0].getBoundingClientRect().top + navSectionArr[highlightedNavIndex][0].clientHeight;
+    const currentSectionDistanceFromTop = navSectionArr[i][0].getBoundingClientRect().top + navSectionArr[i][0].clientHeight;
+    if (highlightedNavIndexDistanceFromTop < 0 && currentSectionDistanceFromTop > 0) {
+      highlightedNavIndex = i; 
     }
+    navSectionArr.forEach((el) => {
+      el[1].classList.remove('active');
+      el[2].classList.remove('active');
+    });
+    navSectionArr[highlightedNavIndex][1].classList.add('active');
+    navSectionArr[highlightedNavIndex][2].classList.add('active');
   }
 }
 
