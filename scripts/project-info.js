7c42fd688266664a1428dcc1b9a1d0f5611f3104
tiago <tiagossansao@gmail.com>
2021-03-06 10:55:31 -0300

    Finished #project-data anchor items's stylization

--- scripts/project-info.js
+++ scripts/project-info.js
@@ -113,12 +113,15 @@ function getProjectHTML({ title, about, techs, showing, github, site }) {
       <aside class="project-data-el">
         ${
           site
-            ? `<a class="project-data-el"  target="_blank" href="${site}">Visit Site</a>`
+            ? `<a class="project-data-el _1"  target="_blank" href="${site}">
+              <svg class="i-svg" height="25" version="1.1" width="25" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 50 50" space="preserve" data-icon="facebook,"><path d="M36.999,36.998H13V13.089L19,13V7h-12v35.998h35.998V27.999h-6V36.998z M25,7l6,6l-9,9l6,6l8.999-9l6,6V7H25z" fill="#ffffff"></path></svg>
+              Visit Site
+            </a>`
             : ''
         }
         ${
           github
-            ? `<a class="project-data-el"  target="_blank" href="${github}"> 
+            ? `<a class="project-data-el  _2"  target="_blank" href="${github}"> 
               <svg class="i-svg" height="25" version="1.1" width="25" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 50 50" space="preserve" data-icon="facebook,"><path d="M36.999,36.998H13V13.089L19,13V7h-12v35.998h35.998V27.999h-6V36.998z M25,7l6,6l-9,9l6,6l8.999-9l6,6V7H25z" fill="#ffffff"></path></svg>
               Visit GitHub</a>`
             : ''
